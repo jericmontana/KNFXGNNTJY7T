cb65ac3ceaff96dc3792fdb1fb24ae9ed63467f3
Jeric Montaña <mntnjrc@gmail.com>
2023-06-03 20:46:26 +0800

    Fix Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 
-import path from 'path'
-
 export default defineConfig({
   build: {
     emptyOutDir: true,
     minify: 'terser',
-    outDir: path.resolve(__dirname, 'production'),
     rollupOptions: {
         output: {
             entryFileNames: 'assets/[name].js',
